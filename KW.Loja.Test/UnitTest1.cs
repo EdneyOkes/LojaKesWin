64d10606fff683f314c16f55c8ae64011c634183
edney.lucas11@yahoo.com.br <edney.lucas11@yahoo.com.br>
2019-03-11 08:30:08 -0300

    Add project files.

---
+++ KW.Loja.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KW.Loja.Test
+{
+    [TestClass]
+    public class UnitTesKesWin
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
