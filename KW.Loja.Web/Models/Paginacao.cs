c616e662a31a82f1a86ed7ca34ab570f3a3b9860
edney.lucas11@yahoo.com.br <edney.lucas11@yahoo.com.br>
2019-03-20 09:19:31 -0300

    Paginacao

---
+++ KW.Loja.Web/Models/Paginacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KW.Loja.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+
+        public int ItensPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+
+
+        public int TotalPagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            }
+
+
+        }
+    }
+}
